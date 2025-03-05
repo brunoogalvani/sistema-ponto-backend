d9115cd388b7907e8f0ad69b8804a193b6dd5be5
brunoogalvani <bthezolin@gmail.com>
2025-03-04 23:32:27 -0300

    GET User by ID

--- src/main/java/com/bruno/SistemaPonto/controllers/UserController.java
+++ src/main/java/com/bruno/SistemaPonto/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.bruno.SistemaPonto.controllers;
 
 import com.bruno.SistemaPonto.dto.RegisterDTO;
+import com.bruno.SistemaPonto.dto.UserDTO;
 import com.bruno.SistemaPonto.entities.User;
 import com.bruno.SistemaPonto.repositories.UserRepository;
 import jakarta.validation.Valid;
@@ -9,12 +10,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
---
+++ src/main/java/com/bruno/SistemaPonto/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package com.bruno.SistemaPonto.dto;
+
+import com.bruno.SistemaPonto.entities.FolhaPonto;
+import com.bruno.SistemaPonto.entities.User;
+import com.bruno.SistemaPonto.entities.UserRole;
+import org.springframework.beans.BeanUtils;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String login;
+    private String password;
+    private UserRole role;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User entity){
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
